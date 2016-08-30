802fb1ee808f305d072880f1f284a8b677684e26
supritkumar shah <suprit7@gmail.com>
2016-08-30 15:56:29 -0400

    php added

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,3 +1,38 @@
-$(document).ready(function($) {
+$(document).ready(function(){
   console.log("connected")
+  // $("#my-menu").mmenu();
+
+  //hiding form
+
+  // $('#send').click(function(){
+  //   console.log('clicked');
+  //   $('#hide').hide();
+  // })
+
+  $(".ajax").submit(function(){
+    var data = {
+      "action" : "test"
+    }
+
+    data = $(this).serialize() + "&" +$.param(data);
+    $.ajax({
+      url: 'response.php',
+      type: 'POST',
+      dataType: 'json',
+      data: data,
+    })
+    .done(function() {
+      console.log("success");
+      alert("success")
+    })
+    .fail(function() {
+      console.log("error");
+    })
+    .always(function() {
+      console.log("complete");
+    });
+
+
+  });
+
 });
